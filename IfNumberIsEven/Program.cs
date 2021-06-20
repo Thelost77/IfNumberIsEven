a5b1752171eb876a8abe32a0cd413ac69303b72a
Thelost77 <wikzie17@gmail.com>
2021-06-20 16:07:22 +0200

    Add complete program

---
+++ IfNumberIsEven/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfNumberIsEven
+{
+    class Program
+    {
+        private static int GetNumber()
+        {
+            while (true)
+            {
+                if(!int.TryParse(Console.ReadLine(), out int givenNumber))
+                {
+                    Console.WriteLine("\nPodałeś błędne dane, podaj liczbę całkowitą!\n");
+                    continue;
+                }
+
+                return givenNumber;
+
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nCześć, podaj mi liczbę całkowitą, a sprawdzę czy jest parzysta.\n");
+            if (GetNumber() % 2 == 0)
+            {
+                Console.WriteLine("\nTwoja liczba jest parzysta!\n");
+            }
+            else
+            Console.WriteLine("\nTwoja liczba jest nieparzysta.\n");
+        }
+    }
+}
